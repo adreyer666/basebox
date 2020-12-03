9583557aa3d27ab79b92cfb000d469e1ed29bf4a
Ansible Writer <ansible@office.adreyer.com>
2020-12-03 19:04:27 +0000

    ensure new kernel starts up

--- centos-8-stream/bootstrap.sh
+++ centos-8-stream/bootstrap.sh
@@ -20,15 +20,20 @@ prep_os(){
     xkeyboard-config hyperv-daemons
   dnf -y install \
     tar fuse procps iproute iptables nftables lsof psmisc curl ca-certificates sudo vim-minimal openssh-clients gnupg2
-  # dnf remove -y  kernel-4.18.0-80.el8.x86_64 kernel-core-4.18.0-80.el8.x86_64 kernel-modules-4.18.0-80.el8.x86_64
   dnf remove -y firewalld-filesystem   ## removes all firewall..
+  dnf -y reinstall kernel kernel-modules kernel-core
+  latest=`rpm -q kernel | sed -e 's/^kernel-//g' | sort -t- -k2n | tail -1`
+  /bin/kernel-install add ${latest} /boot/vmlinuz-${latest}.img
+  sync
+  # dnf remove -y  kernel-4.18.0-80.el8.x86_64 kernel-core-4.18.0-80.el8.x86_64 kernel-modules-4.18.0-80.el8.x86_64
+  ls -al /boot/ /boot/loader/entries/
   dnf clean all
   rm -rf /var/log/anaconda/ /var/cache/man/ /var/cache/dnf/ /var/lib/sss/db/*
 }
 
 user_setup(){
   export PATH=$PATH:/usr/local/bin
-  echo 'PATH=$PATH:/usr/local/bin; export PATH' | sudo tee -a .profile
+  echo 'PATH=$PATH:/usr/local/bin; export PATH' > ${HOME}/.profile
   if test -d /home/deploy;
     then groupmod -g 500 deploy
          usermod -u 500 -g 500 deploy
@@ -44,15 +49,33 @@ user_setup(){
   chown -R deploy: /home/deploy/.ssh
 }
 
-add_builder(){
-  dnf -y install gcc build-essential linux-headers-server
+reset_for_vagrant(){
+  # dnf -y install gcc build-essential linux-headers-server
+  chmod 0700 /home/vagrant/.ssh
+  echo 'vagrant ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/vagrant
+  curl -skL https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub > /home/vagrant/.ssh/vagrant.pub
+  cat /home/vagrant/.ssh/vagrant.pub > /home/vagrant/.ssh/authorized_keys
+  chmod 0600 /home/vagrant/.ssh/authorized_keys
+  cat /home/vagrant/.ssh/authorized_keys
   rm -f /etc/udev/rules.d/70-persistent.net
+  sync; sync; sync;
+}
+
+boot_to_new_kernel(){
+  running=`uname -r`
+  latest=`rpm -q kernel | sed -e 's/^kernel-//g' | sort -t- -k2n | tail -1`
+  test "${running}" = "${latest}" && return
+  test -s "/boot/vmlinuz-${latest}" \
+    && test -s "/boot/initramfs-${latest}.img" \
+    && sync \
+    && kexec --reuse-cmdline --initrd="/boot/initramfs-${latest}.img" "/boot/vmlinuz-${latest}"
 }
 
 #---------------------------------
 
 prep_os
 user_setup
-#add_builder
+reset_for_vagrant
+boot_to_new_kernel
 exit 0
 
